6a2705914c141659374382ec449de1d1ae1bc141
Saad Guessous <guessous_saad@hotmail.com>
2019-02-02 18:05:55 +0200

    added functions project + Ex1 clojure conversion

---
+++ functions/src/functions/core.clj
@@ -0,0 +1,46 @@
+(ns functions.core
+  (:gen-class))
+
+;;;; Ex 1
+;;; Write a predicate NOT-ONEP that return T if its input is anything other than one
+(defun not-onep (n)
+   (not (equal n 1)))
+
+(defn not-onep
+  [n]
+  (not (= n 1)))
+
+
+;;;; Ex 1.16
+;;; Write the predicate NOT-PLUSP that returns T if its input is not greater than zero
+(defun not-plusp (n)
+  (not (> n 0)))
+
+;;;; Ex 1.17
+;;; Some earlier Lisp dialects did not have the EVENP primitive; they only had ODDP. Show how to define EVENP in terms of ODDP.
+(defun my-evenp (n)
+  (not (oddp n)))
+
+;;;; Ex 1.20
+;;; Write XOR, the exclusive-or truth function.
+(defun xor (n m)
+  (not (eq (not n) (not m))));;;; Ex 1.15
+;;; Write a predicate NOT-ONEP that return T if its input is anything other than one
+(defun not-onep (n)
+   (not (equal n 1)))
+
+
+;;;; Ex 1.16
+;;; Write the predicate NOT-PLUSP that returns T if its input is not greater than zero
+(defun not-plusp (n)
+  (not (> n 0)))
+
+;;;; Ex 1.17
+;;; Some earlier Lisp dialects did not have the EVENP primitive; they only had ODDP. Show how to define EVENP in terms of ODDP.
+(defun my-evenp (n)
+  (not (oddp n)))
+
+;;;; Ex 1.20
+;;; Write XOR, the exclusive-or truth function.
+(defun xor (n m)
+  (not (eq (not n) (not m))))
