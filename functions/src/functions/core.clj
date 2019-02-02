0025042f6f208ce4693c398576d87bdfd1a17b3c
Saad Guessous <guessous_saad@hotmail.com>
2019-02-02 18:11:17 +0200

    Added Ex2 clojure conversion

--- functions/src/functions/core.clj
+++ functions/src/functions/core.clj
@@ -1,19 +1,25 @@
 (ns functions.core
   (:gen-class))
 
-;;;; Ex 1
-;;; Write a predicate NOT-ONEP that return T if its input is anything other than one
+;; Ex 1
+;; Write a predicate NOT-ONEP that return T if its input is anything other than one
 (defun not-onep (n)
    (not (equal n 1)))
 
+;; Clojure conversion
 (defn not-onep
   [n]
   (not (= n 1)))
 
 
-;;;; Ex 1.16
-;;; Write the predicate NOT-PLUSP that returns T if its input is not greater than zero
+;; Ex 2
+;; Write the predicate NOT-PLUSP that returns T if its input is not greater than zero
 (defun not-plusp (n)
+  (not (> n 0)))
+
+;; Clojure conversion
+(defn not-plusp
+  [n]
   (not (> n 0)))
 
 ;;;; Ex 1.17
