0e08fc1151848d23102c943c209a45d08318aab1
Saad Guessous <guessous_saad@hotmail.com>
2019-02-03 19:33:35 +0200

    Added Ex4 clojure conversion

--- functions/src/functions/core.clj
+++ functions/src/functions/core.clj
@@ -22,34 +22,21 @@
   [n]
   (not (> n 0)))
 
-;;;; Ex 3
-;;; Some earlier Lisp dialects did not have the EVENP primitive; they only had ODDP. Show how to define EVENP in terms of ODDP.
+;; Ex 3
+;; Some earlier Lisp dialects did not have the EVENP primitive; they only had ODDP. Show how to define EVENP in terms of ODDP.
 (defun my-evenp (n)
   (not (oddp n)))
 
 ;; Clojure conversion. 
 (def my-evenp #(even? %)) 
 
-;;;; Ex 1.20
-;;; Write XOR, the exclusive-or truth function.
-(defun xor (n m)
-  (not (eq (not n) (not m))));;;; Ex 1.15
-;;; Write a predicate NOT-ONEP that return T if its input is anything other than one
-(defun not-onep (n)
-   (not (equal n 1)))
-
-
-;;;; Ex 1.16
-;;; Write the predicate NOT-PLUSP that returns T if its input is not greater than zero
-(defun not-plusp (n)
-  (not (> n 0)))
-
-;;;; Ex 1.17
-;;; Some earlier Lisp dialects did not have the EVENP primitive; they only had ODDP. Show how to define EVENP in terms of ODDP.
-(defun my-evenp (n)
-  (not (oddp n)))
-
-;;;; Ex 1.20
-;;; Write XOR, the exclusive-or truth function.
+;; Ex 4
+;; Write XOR, the exclusive-or truth function.
 (defun xor (n m)
   (not (eq (not n) (not m))))
+
+;; Clojure conversion
+(defn my-xor
+  [a b]
+  (not
+   (= a b)))
